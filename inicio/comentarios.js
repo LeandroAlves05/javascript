96a83b24328db0d64128e6715c8c22ea3f4e9cdb
Leandro Alves <125614595+LeandroAlves05@users.noreply.github.com>
2023-04-18 20:13:03 -0400

    início

---
+++ inicio/comentarios.js
@@ -0,0 +1,13 @@
+// console.log é usado para exibir algo na tela do computador (prompt)!
+
+console.log('Passo #03');
+console.log('Passo #01');
+// console.log('Passo #02');
+
+/*
+ * Esse é um
+ * comentário
+ * de múltiplas
+ * linhas!
+ */
+console.log('Passo #02');
---
+++ inicio/sentenca_1.js
@@ -0,0 +1,4 @@
+console.log("Olá, Mundo!");
+console.log("Bom dia!");
+console.log("Boa tarde!");
+console.log("Boa noite!");
