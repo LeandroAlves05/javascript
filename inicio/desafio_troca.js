1a5b665bb33a856ea77f5e5a044721f06add1586
Leandro Alves <125614595+LeandroAlves05@users.noreply.github.com>
2023-04-20 11:46:09 -0400

    inicio 3

---
+++ inicio/desafio_troca.js
@@ -0,0 +1,14 @@
+let a = 7;
+let b = 94;
+
+console.log("O valor de 'a' é : " + a);
+console.log("O valor de 'b' é : " + b);
+console.log(" ")
+
+let temp = a;
+a = b;
+b = temp; 
+
+// depois da troca... a = 94 e b = 7
+console.log ("Agora o valor de 'a' é: " + a);
+console.log ("E o valor de 'b' é: " + b);
