87b97f3644084e5c7790a65e3cb08733ed8fdbd2
Leandro Alves <leo.vassconceloss@gmail.com>
2023-07-31 17:25:53 -0400

    start em node

--- exercicios-js/fundamentos/inicio/aritmeticos.js
+++ exercicios-js/fundamentos/inicio/aritmeticos.js
@@ -5,3 +5,5 @@ const subtracao = d - b;
 const multiplicacao = a * b;
 const divisao = d / a;
 const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo);
---
+++ exercicios-js/fundamentos/inicio/ternarios.js
@@ -0,0 +1,6 @@
+const result = nota => nota >= 7 ? 'Aprovado' : "Reprovado";
+// nota >= 7 retorna true ou false, se true retorna "aprovado" se false retorna "reprovado"
+
+console.log(result(7.1));
+console.log(result(6.7));
+
