f20e5a3dc94dd85c0ba1ac567d0bdd455f2b80a0
Leandro Alves <125614595+LeandroAlves05@users.noreply.github.com>
2023-08-05 15:04:13 -0400

    Mais Funções

---
+++ teste/aritmeticosEX.js
@@ -0,0 +1,9 @@
+function aritmeticos(a, b) {
+    return `Os Valores foram ${a} e ${b}
+Soma: ${a + b}
+Subtração: ${a - b}
+Multiplicação: ${a * b}
+Divisão Inteira: ${Math.floor(a / b)}`
+}
+
+console.log(aritmeticos(10, 2))
