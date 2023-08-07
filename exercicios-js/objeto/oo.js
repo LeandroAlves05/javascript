9aa8fdcea0bb92619d3160787810678c6a6653f4
Leandro Alves <125614595+LeandroAlves05@users.noreply.github.com>
2023-08-07 16:43:37 -0400

    Objects

---
+++ exercicios-js/objeto/oo.js
@@ -0,0 +1,26 @@
+// CODIGO NÃO EXECUTÁVEL!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+/*
+    Princípios importantes:
+
+    1 - Abtração
+    2 - Encapsulamento
+    3 - Herança (prototype)
+    4 - Polimorfismo
+
+*/
